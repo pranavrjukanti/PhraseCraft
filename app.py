d5975f13e17ae05b6493ac66138cba40c10e3eaf
pranavrjukanti <157426842+pranavrjukanti@users.noreply.github.com>
2026-08-29 10:22:30 -0700

    Refactor page variable assignment and formatting

--- app.py
+++ app.py
@@ -2,29 +2,27 @@ import streamlit as st
 from grammarchecker import grammar_ui
 from PIL import Image
 
-st.set_page_config(page_title="PhraseCraft",page_icon="💬")
+st.set_page_config(page_title="PhraseCraft", page_icon="💬")
 
 st.sidebar.title("PhraseCraft")
-page=st.sidebar.radio("Navigation",["Home","Grammar Checker"])
-##Historical data
-if page=="Home":
-        
-    img=Image.open("chatbot.png")
+page = st.sidebar.radio("Navigation", ["Home", "Grammar Checker"])
+
+if page == "Home":
+
+    img = Image.open("chatbot.png")
     st.image(img)
 
     st.markdown(
         """
     # PhraseCraft
     Your personal AI-powered language coach — here to help you practice, learn, and improve anytime, anywhere.
-    Whether you’re mastering pronunciation, building vocabulary, or preparing for real-world conversations, our chatbot adapts to your level and goals.
+    Whether you're mastering pronunciation, building vocabulary, or preparing for real-world conversations, our chatbot adapts to your level and goals.
     - Practice natural dialogue
     - Get instant feedback and corrections
     - Get guided feedback and step-by-step milestones toward fluency
 
         """
     )
-if page=="Grammar Checker":
+if page == "Grammar Checker":
 
     grammar_ui()
-
-
